b90009f23934f5a3716e261f90704c834350c039
itsanindyak <iankoley04@gmail.com>
2026-05-30 16:41:04 +0530

    add template interface to allow template abstraction

---
+++ mail/renderer.go
@@ -0,0 +1,17 @@
+package mail
+
+import (
+	"bytes"
+	"text/template"
+)
+
+type Template interface {
+	GetTemplate() *template.Template
+	GetSubject() string
+}
+
+func Render(data Template) (string,string,error) {
+	var buf bytes.Buffer
+	err := data.GetTemplate().Execute(&buf, data)
+	return buf.String(), data.GetSubject(), err
+}
